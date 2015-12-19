99c8254db2e027f08f5a4164f70376df49f5968b
Pascal Berger <pascal.berger@ioz.ch>
2015-12-19 16:02:56 +0100

    Implement Restore Git Remote task

--- Tasks/RestoreGitRemote/RestoreGitRemote.ps1
+++ Tasks/RestoreGitRemote/RestoreGitRemote.ps1
@@ -15,4 +15,17 @@ if(!$remoteName)
     throw ("RemoteName parameter must be set.")
 }
 
+import-module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
+
+# Get the original remote URL from the context variable.
+$originalRemoteUrl = Get-TaskVariable -Context $distributedTaskContext -Name IOZ.GitTools.OriginalRemote.$remoteName -Global $FALSE
+
+if ([string]::IsNullOrEmpty($originalRemoteUrl) -eq $true)
+{
+    throw ("Could not find original URL of the remote '$remoteName'. Make sure you have called 'Enable Git Remote Access' Task for the remote '$remoteName' before the 'Restore Git Remote' task.")
+}
+
+git remote set-url $remoteName $originalRemoteUrl
+Write-Verbose "Updated remote for $remoteName is $originalRemoteUrl"
+
 Write-Verbose "Leaving script RestoreGitRemote.ps1"
